9fc6a35f4c16a95224f34a74749fed727d93f53d
cuongtd <trancuong200495@gmail.com>
2021-04-18 10:19:04 +0700

    add screenshot

--- app/src/main/java/com/cuongtd/hackernews/utilities/Helper.kt
+++ app/src/main/java/com/cuongtd/hackernews/utilities/Helper.kt
@@ -1,16 +1,20 @@
 package com.cuongtd.hackernews.utilities
 
 import android.annotation.SuppressLint
+import android.util.Log
 import com.github.marlonlom.utilities.timeago.TimeAgo
 import java.net.URI
 import java.text.SimpleDateFormat
+import java.util.*
 
 class Helper {
     companion object {
         @SuppressLint("SimpleDateFormat")
         fun formatTimeAgo(dateString: String): String {
-            val date = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS").parse(dateString)
-            return TimeAgo.using(date.time)
+            val sdf = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+            sdf.setTimeZone(TimeZone.getTimeZone("UTC"))
+            var  utcDate= sdf.parse(dateString)
+            return TimeAgo.using(utcDate.time)
         }
 
 
